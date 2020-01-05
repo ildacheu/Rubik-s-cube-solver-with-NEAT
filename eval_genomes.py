002c5568077a600ee341b59dab236cfb6010e01e
Maurus Fry <40545498+ildacheu@users.noreply.github.com>
2020-01-05 12:40:09 +0100

    Update fitness function
    The fitness is not anymore computed with the colour of each area, but with the edges and corners of the cube. If they are in the right place and configured the right way, the fitness is increased by 1.

--- eval_genomes.py
+++ eval_genomes.py
@@ -16,6 +16,10 @@ r = 0.8
 
 o = 1
 
+t = ((0, 0), (0, 1), (0, 2))
+r = ((0, 2), (1, 2), (2, 2))
+b = ((2, 2), (2, 1), (2, 0))
+l = ((2, 0), (1, 0), (0, 0))
 def reformatconfigcube(configcube):
     configcolor = []
     for side in configcube:
@@ -75,15 +79,92 @@ def decisionNN(nnOutput, configcube):
         side = configcube[5]
     return direction, side
 
+# def fitnessfunction(configcube):
+#     fitness = 0
+#     for side in range(0, len(configcube)):
+#         for row in range(0, len(configcube[side])):
+#             for plane in range(0, len(configcube[side][row])):
+#                 if configcube[side][row][plane] == cube2.configcubestart[side][row][plane]:
+#                     fitness += 1
+
+#     return fitness
+
+
+def fintessfunctionedges(configcube, config, edgeconfig, edgeconfigstart, side):
+    edge = 0
+    fitness = 0
+    for x in range(0, len(config)):
+        for y in range(0, 2):
+            if configcube[side][config[x][y][0]][config[x][y][1]] == cube2.configcubestart[side][config[x][y][0]][config[x][y][1]]:
+                
+                if edgeconfig[side][edge] == edgeconfigstart[side][edge]:
+                    if edge == 0:
+                        if edgeconfig[side][11] == edgeconfigstart[side][11]:
+                            fitness += 1
+                        edge += 1
+                    elif edge == 3:
+                        if edgeconfig[side][2] == edgeconfigstart[side][2]:
+                            fitness += 1
+                        edge += 1
+                    elif edge == 6:
+                        if edgeconfig[side][5] == edgeconfigstart[side][5]:
+                            fitness += 1
+                        edge += 1
+                    elif edge == 9:
+                        if edgeconfig[side][8] == edgeconfigstart[side][8]:
+                            fitness += 1
+                        edge += 1
+                    
+                    elif edge == 1:
+                        fitness += 1
+                        edge += 2                     
+                    elif edge == 4:
+                        fitness += 1
+                        edge += 2 
+                    elif edge == 7:
+                        fitness += 1
+                        edge += 2 
+                    elif edge == 10:
+                        fitness += 1
+                        edge += 2
+                else:
+                    if edge == 0 or edge == 3 or edge == 6 or edge == 9:
+                        edge += 1
+                    else:
+                        edge += 2                     
+    #print(side)
+    #print(fitness)
+    return fitness
+edges = [[2,1,2],[2,1,0],[4,0,1],[4,2,1],[3,1,0],[3,1,2],[5,0,1],[5,2,1]]
 def fitnessfunction(configcube):
     fitness = 0
+    edgeconfig = [],[],[],[],[],[]
+    edgeconfigstart = [],[],[],[],[],[]
     for side in range(0, len(configcube)):
-        for row in range(0, len(configcube[side])):
-            for plane in range(0, len(configcube[side][row])):
-                if configcube[side][row][plane] == cube2.configcubestart[side][row][plane]:
+        config, rowtotake, opposide = cube2.cube.rotateside(1, configcube[side], configcube)
+        configstart, rowtotake, opposide = cube2.cube.rotateside(1, cube2.configcubestart[side], cube2.configcubestart)
+        for q in range(0, len(rowtotake)):
+            for coordinates in rowtotake[q]:
+                plan = config[q]
+                planstart = configstart[q]
+                edgeconfig[side].append(plan[coordinates[0]][coordinates[1]])
+                edgeconfigstart[side].append(planstart[coordinates[0]][coordinates[1]])
+        
+    fitness = fintessfunctionedges(configcube, [l,t,r,b], edgeconfig, edgeconfigstart, 0) + fintessfunctionedges(configcube, [b,r,t,l], edgeconfig, edgeconfigstart, 1)
+    for edge in range(0, 6):
+        if edge == 0 or edge == 2 or edge == 4 or edge == 6:
+            if configcube[edges[edge][0]][edges[edge][1]][edges[edge][2]] == cube2.configcubestart[edges[edge][0]][edges[edge][1]][edges[edge][2]]:
+                if configcube[edges[edge + 1][0]][edges[edge + 1][1]][edges[edge + 1][2]] == cube2.configcubestart[edges[edge + 1][0]][edges[edge + 1][1]][edges[edge + 1][2]]:
                     fitness += 1
 
-    return fitness 
+
+
+
+    return fitness
+
+
+
+
 
 configc = cube2.randomize()
 
@@ -105,15 +186,20 @@ def eval_genomes(genomes, config):
     
             direction, side = decisionNN(nnOutput, configcube) 
             configcube = game.play(side = side, direction = direction, configcubecube = configcube)
-
             fitness = fitnessfunction(configcube)
+            if gamenumber == 1000:
+                done = True
             if fitness > fitness_current:
                 fitness_current = fitness
-            else:
+            if fitness <= fitness_current:
                 gamenumber += 1
-            if gamenumber == 20:
+                fitness_current = fitness
+            if fitness == 19:
+                cube2.show(configcube)
+                print(fitness)
                 done = True
-            #print(fitness)
+
+            #print(genome_id, fitness)
             genome.fitness = fitness
               
    
